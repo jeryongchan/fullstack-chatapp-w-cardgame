b6e9cf19044e2950c8b9d2cdacd8434f3a40d5ef
Chan Jer Yong <chanjeryong@live.com>
2022-11-27 20:06:28 +0800

    debugging with console logs

--- packages/client/src/components/PrivateRoute.jsx
+++ packages/client/src/components/PrivateRoute.jsx
@@ -3,12 +3,15 @@ import { useSelector } from 'react-redux'
 const { Outlet, Navigate } = require("react-router");
 
 export const useAuth = () => {
+    console.log("a", useSelector(state => state.user.loggedIn))
     // console.log(useSelector(state => state.user.loggedIn))
     return useSelector(state => state.user.loggedIn);
 };
 
 const PrivateRoutes = () => {
+    console.log("b")
     useSocketInit();
+    console.log("c")
     return useAuth() ? <Outlet /> : <Navigate to="/login" />;
 };
 
