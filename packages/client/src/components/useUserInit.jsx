cd725e9548e362028196d5baf543519d288a3e1d
jeryongchan <112857358+jeryongchan@users.noreply.github.com>
2022-12-04 20:23:18 +0800

    Update useUserInit.jsx

--- packages/client/src/components/useUserInit.jsx
+++ packages/client/src/components/useUserInit.jsx
@@ -8,7 +8,7 @@ const useUserInit = () => {
     const navigate = useNavigate();
     const { pathname } = useLocation();
     useEffect(async () => {
-        await fetch("http://localhost:4000/auth/login", {
+        await fetch(`${process.env.REACT_APP_SERVER_URL}/auth/login`, {
             credentials: "include",
         })
             .catch(() => {
