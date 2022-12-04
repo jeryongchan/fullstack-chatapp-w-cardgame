e1080c0d4a3535d8e673cc2f385e335d724bfea5
jeryongchan <112857358+jeryongchan@users.noreply.github.com>
2022-12-04 20:34:41 +0800

    Update socket.js

--- packages/client/src/socket.js
+++ packages/client/src/socket.js
@@ -3,6 +3,7 @@ import { io } from "socket.io-client";
 const socket = new io(process.env.REACT_APP_SERVER_URL, {
     autoConnect: false,
     withCredentials: true,
+    rejectUnauthorized: false,
 });
 
 export default socket;
