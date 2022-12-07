8fcd0320b3f71fd8e787e0105b735c5f8589a059
jeryongchan <112857358+jeryongchan@users.noreply.github.com>
2022-12-07 13:32:48 +0000

    Update fetchAvatar.js

--- packages/client/src/components/utils/fetchAvatar.js
+++ packages/client/src/components/utils/fetchAvatar.js
@@ -1,15 +1,19 @@
 export const fetchAvatar = async (username) => {
     return await fetch(`${process.env.REACT_APP_SERVER_URL}/image/` + username
     ).catch(err => {
+        console.log('err', err)
         return;
     }).then(res => {
+        console.log('res', res)
         if (!res || !res.ok || res.status >= 400) {
             return;
         }
         return res.blob();
     }).then(data => {
+        console.log("data", data)
         if (data) {
             const objectURL = URL.createObjectURL(data);
+            console.log("objectURL", objectURL)
             return objectURL;
         }
         return;
